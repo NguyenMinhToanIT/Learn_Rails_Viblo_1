b79178371e65d1af597b034328a2d0756e7e3647
NguyenMinhToanIT <nguyenminhtoanit01@gmail.com>
2020-09-21 13:31:41 +0700

    show list users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,30 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
 
-    def index
-    end
+  def new
+    @user = User.new
+  end
 
-    def new
-        @user = User.new
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = 'Register success'
+      redirect_to users_path
+    else
+      flash[:danger] = 'Register failed'
+      redirect_to new_user_path
     end
+  end
 
-    def create
-        @user = User.new user_params
-        if @user.save
-            flash[:success] = "Register success"
-            redirect_to users_path
-        else
-            flash[:danger] = "Register failed"
-            redirect_to new_user_path
-        end
-    end
+  def show
+    @user = User.find_by id: params[:id]
+  end
 
-    def show
-        @user = User.find_by id: params[:id]
-    end
+  private
 
-    private
-    def user_params
-        params.require(:user).permit :name, :password, :password_confirmation
-    end
+  def user_params
+    params.require(:user).permit :name, :password, :password_confirmation
+  end
 end
